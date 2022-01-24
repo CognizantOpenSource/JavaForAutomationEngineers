87126472411af06047aa8416267ac2719fa99334
Troy Walsh <92752744+twalshcsv@users.noreply.github.com>
2022-01-24 07:38:09 -0600

    Add GitHub desktop

--- JavaToolingSetup(RunAsAdmin).ps1
+++ JavaToolingSetup(RunAsAdmin).ps1
@@ -1,5 +1,6 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 refreshenv
+choco install github-desktop -y
 choco install openjdk11 -y
 choco install vscode -y
 choco install maven -y
